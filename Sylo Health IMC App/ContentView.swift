b3d47928d6f66d36f230a75c7df7e1ce2f3c0dad
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-07-15 14:58:40 -0600

    Initial Commit

---
+++ Sylo Health IMC App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sylo Health IMC App
+//
+//  Created by Max Vigueras Tovar on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .background(.backgroundaApp)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sylo Health IMC App/Sylo_Health_IMC_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sylo_Health_IMC_AppApp.swift
+//  Sylo Health IMC App
+//
+//  Created by Max Vigueras Tovar on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Sylo_Health_IMC_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
